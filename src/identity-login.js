05a604b810736c96e48c38040c1530ee8013f3e0
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-14 00:06:59 +0800

    test

--- src/identity-login.js
+++ src/identity-login.js
@@ -1,3 +1,6 @@
+const token = '1180083258:AAFcMhA_E2x-R_4euE-uNUs7-YEKokES04Q'
+const {Telegraf} = require('telegraf')
+
 module.handler = async (event, context, callback) => {
   const {identity, user} = context.clientContext;
   try {
