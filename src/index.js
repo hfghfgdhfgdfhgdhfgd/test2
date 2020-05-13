d2b53a85a2286050d0748f5141250773d2b7f978
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-13 19:09:27 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-  const Telegraf = require('telegraf')
-  const token = '1180083258:AAHQDis2A4_OGsdjVBYiXTvhoPNkUVhYJwk'
+const Telegraf = require('telegraf')
+const token = '1180083258:AAHQDis2A4_OGsdjVBYiXTvhoPNkUVhYJwk'
 
-  const bot = new Telegraf.Telegraf(token)
+const bot = new Telegraf.Telegraf(token)
+  
+
+exports.handler = async (event , context ) => {
+  console.log('user connect');
   bot.start((ctx) => ctx.reply('Welcome'))
   bot.help((ctx) => ctx.reply('Send me a sticker'))
   bot.on('sticker', (ctx) => ctx.reply('👍'))
   bot.on('message', (ctx) => ctx.reply(ctx.message))
   bot.hears('hi', (ctx) => ctx.reply('Hey there'))
   bot.launch()
-
-exports.handler = async (event , context ) => {
   return { 
     statusCode : 200 , body : "We are now split testing!"
   }
