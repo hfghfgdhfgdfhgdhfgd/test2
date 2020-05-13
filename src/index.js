b647f51edba15dafbe24623bb3a32d1099783ca4
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-13 19:22:31 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-const Telegraf = require('telegraf')
 const token = '1180083258:AAFcMhA_E2x-R_4euE-uNUs7-YEKokES04Q'
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
 
-const bot = new Telegraf.Telegraf(token)
-  
+const keyboard = Markup.inlineKeyboard([
+  Markup.urlButton('❤️', 'http://telegraf.js.org'),
+  Markup.callbackButton('Delete', 'delete')
+])
+
+const bot = new Telegraf(token)
+bot.start((ctx) => ctx.reply('Hello'))
+bot.help((ctx) => ctx.reply('Help message'))
+bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message, Extra.markup(keyboard)))
+bot.action('delete', ({ deleteMessage }) => deleteMessage())
+bot.launch()
 
 exports.handler = async (event , context ) => {
   try {
