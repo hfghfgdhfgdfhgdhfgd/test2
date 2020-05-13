03b962130d97d962b85658cc3bbbd12b458e8bea
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-13 19:14:01 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const Telegraf = require('telegraf')
-const token = '1180083258:AAHQDis2A4_OGsdjVBYiXTvhoPNkUVhYJwk'
+const token = '1180083258:AAFcMhA_E2x-R_4euE-uNUs7-YEKokES04Q'
 
 const bot = new Telegraf.Telegraf(token)
   
 
 exports.handler = async (event , context ) => {
-  console.log('user connect');
-  bot.start((ctx) => ctx.reply('Welcome'))
-  bot.help((ctx) => ctx.reply('Send me a sticker'))
-  bot.on('sticker', (ctx) => ctx.reply('👍'))
-  bot.on('message', (ctx) => ctx.reply(ctx.message))
-  bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-  bot.launch()
+  try {
+    console.log('user connect');
+    bot.start((ctx) => ctx.reply('Welcome'))
+    bot.help((ctx) => ctx.reply('Send me a sticker'))
+    bot.on('sticker', (ctx) => ctx.reply('👍'))
+    bot.on('message', (ctx) => ctx.reply(ctx.message))
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    bot.launch()
+  } catch (err) {
+    console.log('ERROR:' + err);
+  }
   return { 
     statusCode : 200 , body : "We are now split testing!"
   }
