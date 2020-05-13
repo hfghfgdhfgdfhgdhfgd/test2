b69c4ba4c766914885b425c323cab56911bf0e21
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-14 00:38:03 +0800

    test

--- src/identity-signup.js
+++ src/identity-signup.js
@@ -7,9 +7,17 @@ exports.handler = async (event, context, callback) => {
     const bot = new Telegraf(process.env.telegramKey)
     await bot.telegram.sendMessage(process.env.notificationChatId, 'функция включилась ' + 
     JSON.stringify(user) + ' ' + JSON.stringify(identity));
-    callback(null, { statusCode: 200, body: '{}' })
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(
+        {app_metadata: {authorization: {roles: ["admin"]}}}
+      )
+    })
     console.log('SUCSESS')
   } catch (err) {
     console.log('ERROR', JSON.stringify(err));
+  }
+  return {
+    statusCode: 200
   }
 }
