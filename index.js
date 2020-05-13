8652ca712f1071a21a4b5e4474963a587c60b485
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-13 18:24:10 +0800

    test

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const { Telegraf } = require('telegraf')
+  const Telegraf = require('telegraf')
+  const token = '1180083258:AAHQDis2A4_OGsdjVBYiXTvhoPNkUVhYJwk'
 
-exports.handler = async (event , context ) => {
-  const bot = new Telegraf(event.queryStringParameters.test)
-  console.log('send message')
+  const bot = new Telegraf.Telegraf(token)
   bot.start((ctx) => ctx.reply('Welcome'))
   bot.help((ctx) => ctx.reply('Send me a sticker'))
   bot.on('sticker', (ctx) => ctx.reply('👍'))
   bot.on('message', (ctx) => ctx.reply(ctx.message))
   bot.hears('hi', (ctx) => ctx.reply('Hey there'))
   bot.launch()
+
+exports.handler = async (event , context ) => {
   return { 
     statusCode : 200 , body : "We are now split testing!"
   }
