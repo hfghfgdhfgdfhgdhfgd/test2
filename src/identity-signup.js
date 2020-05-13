4e136b86ab5b2b75069f59e05e70a2793a7431e2
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-14 00:29:10 +0800

    test

--- src/identity-signup.js
+++ src/identity-signup.js
@@ -1,4 +1,3 @@
-const token = '1180083258:AAFcMhA_E2x-R_4euE-uNUs7-YEKokES04Q'
 const {Telegraf} = require('telegraf')
 
 exports.handler = async (event, context, callback) => {
@@ -8,11 +7,9 @@ exports.handler = async (event, context, callback) => {
     const bot = new Telegraf(process.env.telegramKey)
     await bot.telegram.sendMessage(process.env.notificationChatId, 'функция включилась ' + 
     JSON.stringify(user) + ' ' + JSON.stringify(identity));
+    callback(null, { statusCode: 200, body: '{}' })
     console.log('SUCSESS')
   } catch (err) {
     console.log('ERROR', JSON.stringify(err));
-  }
-  return {
-    statusCode : 200
   }
 }
